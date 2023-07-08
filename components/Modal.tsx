76bc4bba10f231a339afde4edbd463538e03a103
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-07-08 17:23:33 -0400

    Modal setup

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,6 +1,22 @@
 "use client";
 
-const Modal = () => {
+// -> Beyond codebase
+import * as Dialog from "@radix-ui/react-dialog";
+import { ReactNode } from "react";
+import { IoMdClose } from "react-icons/io";
+// -> Within codebase
+
+type ModalProps = {
+  isOpen: boolean;
+  onChange: (open: boolean) => void;
+  title: string;
+  description: string;
+  children: ReactNode;
+}
+
+const Modal = (props: ModalProps) => {
+  const { isOpen, onChange, title, description, children } = props;
+  
   return (
     <div>Modal</div>
   )
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -16,7 +16,14 @@ const ModalProvider = () => {
 
   return (
     <>
-      <Modal />
+      <Modal
+        title="Test Title"
+        description="Test Description"
+        isOpen
+        onChange={() => {}}
+      >
+        Test children content
+      </Modal>
     </>
   );
 }
