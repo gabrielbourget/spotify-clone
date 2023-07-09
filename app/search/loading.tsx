4110e14174c8546b3c9f35879f9f0909880bbf84
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-07-09 19:26:46 -0400

    loading and error states

---
+++ app/search/loading.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { BounceLoader } from "react-spinners";
+import Box from "@/components/Box";
+
+const loading = () => {
+  return (
+    <Box className="h-full flex items-center justify-center">
+      <BounceLoader color="#22C55E" size={40} />
+    </Box>
+  )
+}
+
+export default loading;
