792875fbcec432cbd9a87ceb3339db28689af848
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-07-09 18:21:26 -0400

    add use-sound, fix typo

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import useGetSongById from "@/hooks/useGetSongById";
-import useLoadSongUrl from "@/hooks/useLoadSongURL";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import usePlayer from "@/hooks/usePlayer";
 import PlayerContent from "./PlayerContent";
 
