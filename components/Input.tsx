45cb45cd8400d8fd6f7289b7cdc541a0a24162a7
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-07-08 22:40:59 -0400

    Input component

---
+++ components/Input.tsx
@@ -0,0 +1,22 @@
+import React, { forwardRef } from 'react'
+import { twMerge } from 'tailwind-merge';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>((props: InputProps, ref) => {
+  const { className, type, disabled, ...rest } = props;
+
+  return (
+    <input
+      ref={ref}
+      type={type}
+      className={twMerge(`flex w-full rounded-md bg-neutral-700 border border-transparent px-3 py-3 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 disabled:cursor-not-allowed disalbed:opacity-50 focus:outline-none`, className)}
+      disabled={disabled}
+      {...rest}
+    />
+  )
+});
+
+Input.displayName = "Input";
+
+export default Input
--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 // -> Beyond codebase
+import { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 // -> Within codebase
 import useUploadModal from "@/hooks/useUploadModal";
+import Input from "./Input";
 import Modal from "./Modal";
 
 const UploadModal = () => {
   const { isOpen, onClose } = useUploadModal();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const { register, handleSubmit, reset } = useForm<FieldValues>({
     defaultValues: {
@@ -40,7 +43,12 @@ const UploadModal = () => {
       <form
         onSubmit={handleSubmit(onSubmit)}
       >
-        
+        <Input
+          id="title"
+          disabled={isLoading}
+          {...register("title", { required: true })}
+          placeholder="Song Title"
+        />
       </form>
     </Modal>
   )
