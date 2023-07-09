699298aabbca7b0a55f7d95470b7b757ecfb41f9
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-07-09 18:40:05 -0400

    styles

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,12 +5,13 @@ import { usePathname } from "next/navigation";
 import { ReactNode, useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
+import { twMerge } from "tailwind-merge";
 // -> Within codebase
-import Box from "./Box";
-import SidebarItem from "./SidebarItem";
-import Library from "./Library";
+import usePlayer from "@/hooks/usePlayer";
 import { Song } from "@/types";
-
+import Box from "./Box";
+import Library from "./Library";
+import SidebarItem from "./SidebarItem";
 
 type SidebarProps = {
   children: ReactNode;
@@ -20,6 +21,7 @@ type SidebarProps = {
 const Sidebar = (props: SidebarProps) => {
   const { songs, children } = props;
   const pathname = usePathname();
+  const player = usePlayer();
 
   const routes = useMemo(() => [
     {
@@ -37,7 +39,7 @@ const Sidebar = (props: SidebarProps) => {
   ], [pathname]);
 
   return (
-    <div className="flex h-full">
+    <div className={twMerge(`flex h-full`, player.activeId && "h-[calc(100%-80px)]")}>
       <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
         <Box>
           <div className="flex flex-col gap-y-4 px-5 py-4">
