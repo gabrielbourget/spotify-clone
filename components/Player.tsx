69c14ec9a75f6a43ef2f23ab5ac2799f34f5b1b3
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-07-09 17:07:56 -0400

    scaffold audio player

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,16 +1,21 @@
 "use client";
 
 import useGetSongById from "@/hooks/useGetSongById";
+import useLoadSongUrl from "@/hooks/useLoadSongURL";
 import usePlayer from "@/hooks/usePlayer";
 
 const Player = () => {
   const player = usePlayer();
   const { song } = useGetSongById(player.activeId);
 
-  // const songUrl = useLoadSongUrl(song);
+  const songUrl = useLoadSongUrl(song!);
+  
+  if (!song || !songUrl || !player.activeId) return null;
 
   return (
-    <div>Player</div>
+    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+      Player
+    </div>
   )
 }
 
