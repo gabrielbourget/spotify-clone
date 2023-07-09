fe328731848406146d053c48ba8582041aa09f57
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-07-09 15:42:14 -0400

    Search Input

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,9 +1,31 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import qs from "query-string";
+import { useEffect, useState } from "react";
+
+import useDebounce from "@/hooks/useDebounce";
+import Input from "./Input";
+
 const SearchInput = () => {
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  useEffect(() => {
+    const query = { title: debouncedValue };
+    const url = qs.stringifyUrl({ url: "/search", query });
+
+    router.push(url);
+  }, [debouncedValue, router])
+  
   return (
-    <div>SearchInput</div>
-  )
+    <Input
+      placeholder="What are you searching for?"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
 }
 
 export default SearchInput
